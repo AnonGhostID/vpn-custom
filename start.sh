e2ac19e0b5c39a10b298dd844b2c3449f302a62b
AnonGhostID <tnadam@pm.me>
2024-10-24 14:55:00 +0700

    plis

--- start.sh
+++ start.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
-# # Download and extract Tailscale
-# wget https://pkgs.tailscale.com/stable/tailscale_1.76.1_amd64.tgz && \
-#     tar xzf tailscale_1.76.1_amd64.tgz --strip-components=1
+# Download and extract Tailscale
+wget https://pkgs.tailscale.com/stable/tailscale_1.76.1_amd64.tgz && \
+    tar xzf tailscale_1.76.1_amd64.tgz --strip-components=1
 
 # # Add Tailscale binaries to PATH
 # export PATH=$PATH:$(pwd)
-
-
 # Create necessary directory for Tailscale
-mkdir -p /var/run/tailscale
+# mkdir -p /var/run/tailscale
 
 # Start Tailscale
-tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=ephermeral-vpn-${PORT} --advertise-exit-node
+
+# tailscaled -no-logs-no-support --socket=/tmp/tailscaled.sock --tun=userspace-networking --outbound-http-proxy-listen=0.0.0.0:2000 --socks5-server=0.0.0.0:2001 &
+tailscaled -no-logs-no-support --socket=/tmp/tailscaled.sock --tun=userspace-networking --socks5-server=localhost:1055 &
+
+until tailscale --socket=/tmp/tailscaled.sock up --authkey=${TAILSCALE_AUTHKEY} --hostname=ephermeral-vpn-${PORT} --advertise-exit-node
+do
+    sleep 1
+done
+
 echo Tailscale started
 
 # Set proxy and start Gunicorn
