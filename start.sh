0837f92ab7bfcd7c378572fabd1c1a3f56e10f93
AnonGhostID <tnadam@pm.me>
2024-10-24 15:39:40 +0700

    asd

--- start.sh
+++ start.sh
@@ -5,11 +5,13 @@ wget https://pkgs.tailscale.com/stable/tailscale_1.76.1_amd64.tgz && \
     tar xzf tailscale_1.76.1_amd64.tgz --strip-components=1
 
 # # Add Tailscale binaries to PATH
-# export PATH=$PATH:$(pwd)
+export PATH=$PATH:$(pwd)
 # Create necessary directory for Tailscale
 # mkdir -p /var/run/tailscale
 
 # Start Tailscale
+
+ls 
 
 # tailscaled -no-logs-no-support --socket=/tmp/tailscaled.sock --tun=userspace-networking --outbound-http-proxy-listen=0.0.0.0:2000 --socks5-server=0.0.0.0:2001 &
 tailscaled -no-logs-no-support --socket=/tmp/tailscaled.sock --tun=userspace-networking --socks5-server=localhost:1055 &
