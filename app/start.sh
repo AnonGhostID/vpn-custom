778a4c07c2fc434bb47e5caa4d8b0c8b20888eed
AnonGhostID <tnadam@pm.me>
2024-10-24 14:23:10 +0700

    s

--- app/start.sh
+++ app/start.sh
@@ -4,8 +4,8 @@
 wget https://pkgs.tailscale.com/stable/tailscale_1.76.1_amd64.tgz && \
     tar xzf tailscale_1.76.1_amd64.tgz --strip-components=1
 
-/app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=ephermeral-vpn-${PORT} --advertise-exit-node
+tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=ephermeral-vpn-${PORT} --advertise-exit-node
 echo Tailscale started
 ALL_PROXY=socks5://localhost:1055/
 gunicorn --bind 0.0.0.0:${PORT} --workers 2 --threads 2 app.wsgi:app
